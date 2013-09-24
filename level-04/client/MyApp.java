a59fa127b564a86713f10b4095f7578e33e7a3ed
Tsuyoshi Watanabe <tsuyoshi_watanabe@ywatanabe.nashua>
2013-09-24 11:58:43 -0400

    added level 04

---
+++ level-04/client/MyApp.java
@@ -0,0 +1,15 @@
+package testapp;
+
+import testlib.Util;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+		
+		Util util = new Util();
+		System.out.println(util.getMessage());
+	}
+
+}
