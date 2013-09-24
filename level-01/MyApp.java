3e67e867b0e106e17dd811f5663191229bf98e34
Tsuyoshi Watanabe <tsuyoshi_watanabe@ywatanabe.nashua>
2013-09-24 10:52:44 -0400

    adding level 01

---
+++ level-01/MyApp.java
@@ -0,0 +1,11 @@
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		
+		Util util = new Util();
+		System.out.println(util.getMessage());
+	}
+
+}
---
+++ level-01/Util.java
@@ -0,0 +1,8 @@
+
+public class Util {
+
+	public String getMessage() {
+		return "I am Util.";
+	}
+}
+
